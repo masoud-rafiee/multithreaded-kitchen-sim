818cff9ffd2a2019692015ab3923f5a121b85a1e
Masoud Rafiee <masoudrafi97@gmail.com>
2025-04-04 02:32:06 -0400

    Initial commit

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cooking"},{"p":"<Unnamed>","l":"CookTaskThread"},{"p":"<Unnamed>","l":"MultiThreadedCook"}];updateSearchResults();
